ace95a8a58e7c66aa8d123a266489d96cbe282e0
jolitti <marcodeltrionfo@gmail.com>
2021-12-01 14:54:28 +0100

    Works, but is too slow

--- graph/shortestpath1.py
+++ graph/shortestpath1.py
@@ -1,20 +1,30 @@
-#TODO: one-way only
+import sys
+def eprint(*args):
+    for a in args:
+        sys.stderr.write(str(a))
+        sys.stderr.write(" ")
+    sys.stderr.write("\n")
+
+#eprint(*[1,2,3,4])
+
 
 from queue import PriorityQueue
 
 def link(a,b,links):
-    if a not in links:
+    linkList = links.get(a,None)
+
+    if linkList is None:
         links[a]=[b]
     else:
         links[a].append(b)
-    if b not in links:
+    """ if b not in links:
         links[b]=[a]
     else:
-        links[b].append(a)
+        links[b].append(a) """
 
 def getWeight(a,b,weigths):
     if a==b: return 0
-    return weigths[frozenset([a,b])]
+    return weigths[(a,b)]
 
 while True:
     n,m,queries,start = map(int,input().split())
@@ -29,20 +39,25 @@ while True:
     for i in range(m):
         a,b,w = map(int,input().split())
         link(a,b,links)
-        weigths[frozenset([a,b])] = w
+        weigths[(a,b)] = w
 
     q = PriorityQueue()
     q.put((0,start))
     while not q.empty():
+        eprint(q.queue)
         weigth, index = q.get()
-        for x in links[index]:
-            if distances[x]<0:
-                newWeigth = weigth+getWeight(index,x,weigths)
-                q.put((newWeigth,x))
+        linkList = links.get(index,None)
+        if linkList is not None:
+            for x in linkList:
+                if distances[x]<0:
+                    newWeigth = weigth+getWeight(index,x,weigths)
+                    q.put((newWeigth,x))
         if distances[index]<0:
             distances[index]=weigth
     
     #print(*distances)
 
     for i in range(queries):
-        print(distances[int(input())])
+        d = distances[int(input())]
+        if d<=-1: d="Impossible"
+        print(d)
